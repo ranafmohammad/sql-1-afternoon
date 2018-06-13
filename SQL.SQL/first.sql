ebb136f78d547cbee0a1c33acf46155f4151e65a
Rana Mohammad <ranamohammad@ranas-mbp.attlocal.net>
2018-06-13 18:47:22 -0500

    first

---
+++ SQL.SQL/first.sql
@@ -0,0 +1,48 @@
+ CREATE TABLE Person ( ID INTEGER PRIMARY KEY AUTOINCREMENT, Name string, Age integer, Height integer, City string, FavoriteColor string );
+ 
+ 
+ 
+  INSERT INTO Person (Name, Age, Height, City, FavoriteColor) VALUES ("Jhon Jhon", 23, 180, "Staten Island", "Red"),
+ ("Smith Smith", 25, 185, "Dallas", "Pink" ), 
+ ("Jhon Smith", 28, 183, "Brooklyn", "Black"),
+ ("Smith Jhon", 30, 190, "Queens", "Purple"),
+ ("SJ Jhon Smith", 29,192, "Long Island", "Grey"); 
+
+
+  SELECT * FROM Person ORDER BY Height DESC;
+
+
+SELECT * FROM Person ORDER BY Height ASC;
+
+
+SELECT * FROM Person ORDER BY Age  DESC;
+
+
+SELECT * FROM Person WHERE Age > 20;
+
+
+SELECT * FROM Person WHERE Age = 18;
+
+
+SELECT * FROM Person WHERE Age < 20 OR Age > 30;
+
+
+SELECT * FROM Person WHERE Age != 27;
+
+
+SELECT * FROM Person WHERE FavoriteColor != "Red";
+
+
+SELECT * FROM Person WHERE FavoriteColor != "Red" AND FavoriteColor != "Blue";
+
+
+SELECT * FROM Person WHERE FavoriteColor = "Oange" OR FavoriteColor = "Green";
+
+
+SELECT * FROM Person WHERE FavoriteColor IN ( "Orange", "Green", "Blue" );
+
+
+SELECT * FROM Person WHERE FavoriteColor IN ( "yellow", "purple" );
+
+
+
